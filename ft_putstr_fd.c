e23cee3aee563e141f50cb2e2e57b8fb919c0bae
yutaaa-10 <kuro0308yuu@gmail.com>
2026-04-24 08:59:31 +0900

    putstr,nb,

---
+++ ft_putstr_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukurosa <yukurosa@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/24 08:34:50 by yukurosa          #+#    #+#             */
+/*   Updated: 2026/04/24 08:43:46 by yukurosa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char	str[] = "hello";
+
+	ft_putstr_fd(str, 1);
+	return (0);
+}
